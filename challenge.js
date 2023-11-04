caa285374208e8d88c84a2f1e77aa00016d0212a
NIMCOO <nimco77001@gmail.com>
2023-11-04 06:41:06 +0000

    Finshed

--- challenge.js
+++ challenge.js
@@ -1,2 +1,11 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+const A =3
+const B =6
+const c =300
+if(A>B<300){
+    console. log("The largest number out of the three numbers is c")
+}
+
+
